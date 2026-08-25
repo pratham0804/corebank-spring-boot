a61e360526a0074b96e1a3ef24dbda2341fea842
Prathamesh <prathameshjangle425@gmail.com>
2026-08-25 13:15:02 +0530

    Initial Spring boot project setup

---
+++ src/main/java/com/corebank/entity/Customer.java
@@ -0,0 +1,60 @@
+package com.corebank.entity;
+
+import org.hibernate.Hibernate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "customers")
+public class Customer {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+   
+
+  private String name;
+   
+
+  private String email;
+
+   
+
+  public Customer(){
+//  Hibernate reads a database row and creates a Java object from it. The no-argument constructor helps Hibernate create that object.
+  }
+
+  public Customer(String name,String email){
+      this.name = name;
+      this.email = email;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+     this.name = name;
+  }
+
+
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+     this.email = email;
+  }
+
+   
+}
