9b58c02cba30f9f9c965ab282c7c6960abee634d
Prathamesh <prathameshjangle425@gmail.com>
2026-09-07 10:00:52 +0000

    Logging Filter added

--- src/main/java/com/corebank/CorebankApplication.java
+++ src/main/java/com/corebank/CorebankApplication.java
@@ -2,9 +2,11 @@ package com.corebank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.server.servlet.context.ServletComponentScan;
 
 
 @SpringBootApplication
+@ServletComponentScan 
 public class CorebankApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/corebank/filter/LoggingFilter.java
@@ -0,0 +1,39 @@
+package com.corebank.filter;
+
+import java.io.IOException;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.FilterConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.annotation.WebFilter;
+
+@WebFilter("/*")
+public class LoggingFilter implements Filter {
+
+   @Override
+   public void init(FilterConfig filterConfig) throws ServletException {
+     
+       System.out.println("Filter initalized");
+   }
+
+
+
+   @Override
+   public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        
+        System.out.println("Request entered , about to hit the application code");
+
+       chain.doFilter(request, response);
+
+       System.out.println("Reponse returned from application code");
+   }
+
+   @Override
+   public void destroy() {
+       
+       System.out.println("Filter destroyed");
+   }
+}
