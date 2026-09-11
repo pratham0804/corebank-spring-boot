b15c6fbcf076d2eea204ab572bf99f6df7f9e833
Prathamesh <prathameshjangle425@gmail.com>
2026-09-11 12:18:45 +0530

    Added ResponseEntity for both Controllers

--- src/main/java/com/corebank/controller/AccountController.java
+++ src/main/java/com/corebank/controller/AccountController.java
@@ -4,6 +4,10 @@ import com.corebank.service.AccountService;
 import com.corebank.entity.Account;
 
 import org.springframework.web.bind.annotation.RequestBody;
+
+import javax.security.auth.login.AccountNotFoundException;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,8 +22,10 @@ public class AccountController {
     }
 
     @PostMapping("/addAccount")
-    public Account AddAccount(@RequestBody Account account){
-       return accountService.saveAccount(account);
+    public ResponseEntity<Account> AddAccount(@RequestBody Account account){
+
+      Account acc = accountService.saveAccount(account);
+      return ResponseEntity.status(201).body(acc);
     }
 
 
--- src/main/java/com/corebank/controller/CustomerController.java
+++ src/main/java/com/corebank/controller/CustomerController.java
@@ -1,5 +1,7 @@
 package com.corebank.controller;
 
+import org.springframework.data.repository.config.CustomRepositoryImplementationDetector;
+import org.springframework.http.ResponseEntity;
 import com.corebank.entity.Customer;
 import com.corebank.service.CustomerService;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,8 +20,17 @@ public class CustomerController {
     }
 
     @PostMapping("/addCustomer")
-    public Customer saveCustomer(@RequestBody Customer customer){
-       return customerService.saveCustomer(customer);
+    public ResponseEntity<Customer> saveCustomer(@RequestBody Customer customer){
+      //  Customer cus = customerService.saveCustomer(customer); // this is important , customer object we are receiving has id as null , and using it for returning in body of response entity will give u only name and email and id as null , if we want system generated id too in our response , we store the returned object and then pass it to body, not the object we are getting from request body... 
+      // imp : i tested this , but still i got correct output 
+      // Reason : save() returns the saved entity, and we should generally use that returned value rather than assuming the input reference is the object we should work with.
+
+  
+      //  customerService.saveCustomer(customer);
+      //  return ResponseEntity.status(201).body(customer);
+
+      Customer cus = customerService.saveCustomer(customer);
+      return ResponseEntity.status(201).body(cus);
     }
 
     
