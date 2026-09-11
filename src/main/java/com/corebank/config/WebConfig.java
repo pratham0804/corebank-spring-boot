eadd87e30e23b617e93f1791ab1f202054824927
Prathamesh <prathameshjangle425@gmail.com>
2026-09-11 11:55:42 +0530

    Random

--- src/main/java/com/corebank/config/WebConfig.java
+++ src/main/java/com/corebank/config/WebConfig.java
@@ -1,8 +1,6 @@
 package com.corebank.config;
 
 import com.corebank.interceptor.LoggingInterceptor;
-
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
